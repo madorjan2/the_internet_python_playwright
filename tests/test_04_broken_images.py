fac67c29c14e2da28c019a6d10b29c4832657fc4
madorjan2 <76928045+madorjan2@users.noreply.github.com>
2025-04-21 23:30:32 +0200

    test 4

---
+++ tests/test_04_broken_images.py
@@ -0,0 +1,26 @@
+import pytest
+
+from utils.base_test import BaseTest
+
+
+class TestBrokenImages(BaseTest):
+	page_url = '/broken_images'
+
+	@pytest.mark.parametrize(
+		'index, expected',
+		[
+			(1, True),
+			(2, True),
+			(3, False),
+		],
+	)
+	def test_image_is_broken(self, def_page, index, expected):
+		xpath_selector = f'//div[@id="content"]//img[{index}]'
+		is_broken = self.page.evaluate(
+			"""(selector) => {
+				const img = document.evaluate(selector, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+				return img ? img.naturalWidth === 0 : true;
+			}""",
+			xpath_selector,
+		)
+		assert is_broken == expected
