67d3f167da273efb09a52d60245121409b004ec4
madorjan2 <76928045+madorjan2@users.noreply.github.com>
2025-04-21 03:56:52 +0200

    added self.page capabilities + test 2

--- tests/test_02_add_or_remove_elements.py
+++ tests/test_02_add_or_remove_elements.py
@@ -8,12 +8,12 @@ class TestAddRemoveElement(BaseTest):
 	def add_element(self):
 		self.page.get_by_role('button', name='Add Element').click()
 
-	def get_number_of_delete_buttons(self):
-		return len(self.page.get_by_role('button', name='Delete').all())
+	def assert_number_of_delete_buttons(self, num):
+		expect(self.page.get_by_role('button', name='Delete')).to_have_count(num)
 
 	def test_add_element(self, def_page):
 		self.add_element()
-		assert self.get_number_of_delete_buttons() == 1
+		self.assert_number_of_delete_buttons(1)
 		self.add_element()
 		self.add_element()
-		assert self.get_number_of_delete_buttons() == 3
+		self.assert_number_of_delete_buttons(3)
