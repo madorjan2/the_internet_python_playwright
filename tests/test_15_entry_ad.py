4a908a3f489df43e5cab5eb00b94ae7f3eeed2ce
madorjan2 <76928045+madorjan2@users.noreply.github.com>
2025-04-26 23:01:36 +0200

    test 15 fix

--- tests/test_15_entry_ad.py
+++ tests/test_15_entry_ad.py
@@ -7,16 +7,18 @@ from utils.base_test import BaseTest
 class TestEntryAd(BaseTest):
 	page_url = '/entry_ad'
 
-	@pytest.mark.flaky(reruns=5)
+	@pytest.mark.flaky(reruns=3)
 	def test_entry_ad(self, def_page):
-		self.page.locator('.modal-footer p').click()
+		p_close = self.page.locator('.modal-footer p')
+		p_close.click()
 		modal = self.page.locator('.modal')
 		modal.wait_for(state='hidden')
+		expect(modal).not_to_be_visible()
+
+		self.page.reload()
 		expect(modal).not_to_be_visible()
 
 		self.page.get_by_role('link', name='click here').click()
 		modal.wait_for(state='visible')
 		expect(modal).to_be_visible()
 
-		self.page.reload()
-		expect(modal).to_be_visible()
